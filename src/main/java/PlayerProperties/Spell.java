de5715c61e70b6c94808477d82fd638014b68951
GerCassa <gercassa@gmail.com>
2018-10-18 11:19:09 +0100

    Spell class created

--- src/main/java/PlayerProperties/Spell.java
+++ src/main/java/PlayerProperties/Spell.java
@@ -1,4 +1,25 @@
 package PlayerProperties;
 
 public class Spell {
+
+    private String name;
+    private int damage;
+    private int level;
+
+    public Spell(String name, int damage, int level) {
+        this.name = name;
+        this.damage = damage;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getLevel() { return level; }
+
 }

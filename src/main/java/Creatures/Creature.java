0c8549017dad804fd61b83ca062437594db7908a
GerCassa <gercassa@gmail.com>
2018-10-18 11:24:51 +0100

    Creature class modified and improved

--- src/main/java/Creatures/Creature.java
+++ src/main/java/Creatures/Creature.java
@@ -1,4 +1,32 @@
 package Creatures;
 
 public class Creature {
+
+    private String name;
+    private int hp;
+    private int damage;
+    private int level;
+
+    public Creature(String name, int hp, int damage, int level) {
+        this.name = name;
+        this.hp = hp * level;
+        this.damage = damage * level;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getLevel() {
+        return level;
+    }
 }

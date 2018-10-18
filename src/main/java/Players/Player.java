87fea34545d794cc3152b60049fb4fb064af3fc0
GerCassa <gercassa@gmail.com>
2018-10-18 10:52:08 +0100

    player created

---
+++ src/main/java/Players/Player.java
@@ -0,0 +1,20 @@
+package Players;
+
+public abstract class Player {
+
+    private String name;
+    private int hp;
+
+    public Player(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+}

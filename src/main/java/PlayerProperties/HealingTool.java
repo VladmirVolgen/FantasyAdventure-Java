7f14606dd0940cb921bb4cb2e1ed1b189a650e89
GerCassa <gercassa@gmail.com>
2018-10-18 11:21:14 +0100

    HealTool class created

--- src/main/java/PlayerProperties/HealingTool.java
+++ src/main/java/PlayerProperties/HealingTool.java
@@ -1,4 +1,20 @@
 package PlayerProperties;
 
 public class HealingTool {
+
+    private String name;
+    private int healPower;
+
+    public HealingTool(String name, int healPower) {
+        this.name = name;
+        this.healPower = healPower;
+    }
+
+    public int getHealPower() {
+        return healPower;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
